e942bfb5d04597e57ecece26e22ce7d03222858b
marcoaaguiar <marcoaaguiar@gmail.com>
2020-06-07 17:01:01 -0300

    Removed external references to WSAuthTicket.key, insteade uses ticket

--- channels_ws_auth/models.py
+++ channels_ws_auth/models.py
@@ -15,7 +15,7 @@ class WSAuthTicket(models.Model):
         verbose_name = _("WS Auth Ticket")
         verbose_name_plural = _("WS Auth Tickets")
 
-    key = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    key = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created = models.DateTimeField(_("Created"), auto_now_add=True)
 
--- tests/test_views.py
+++ tests/test_views.py
@@ -4,8 +4,6 @@ test_channels-ws-auth
 
 Tests for `channels-ws-auth` models module.
 """
-import pytest
-from rest_framework.test import APIRequestFactory, force_authenticate
 from channels_ws_auth.models import WSAuthTicket
 
 
@@ -20,19 +18,19 @@ def test_request(api_client_with_credentials):
 
 
 def test_request_creates(api_client_with_credentials):
-    response = api_client_with_credentials.post("/")
+    api_client_with_credentials.post("/")
     assert WSAuthTicket.objects.count() == 1
 
 
 def test_request_creates_with_user(user, api_client_with_credentials):
-    response = api_client_with_credentials.post("/")
+    api_client_with_credentials.post("/")
     ticket = WSAuthTicket.objects.last()
     assert ticket.user == user
 
 
-def test_request_body_content(user, api_client_with_credentials):
+def test_request_body_content(api_client_with_credentials):
     response = api_client_with_credentials.post("/")
     ticket = WSAuthTicket.objects.last()
     body = response.json()
-    assert "key" in body
-    assert body["key"] == str(ticket.key)
+    assert "ticket" in body
+    assert body["ticket"] == str(ticket.key)
